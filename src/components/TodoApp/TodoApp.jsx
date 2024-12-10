a95d299be0053d5bf20c775bfba0438f45d6ea42
Victor Terra <victorbterra@gmail.com>
2024-12-10 12:27:08 -0300

    feat: - Construção da lógica do projeto - Função para adicionar tarefa - condição para não adicionar tarefa vazia - adicionando a nova tarefa à lista de tarefas e limpa o input - Criando lógica para mostrar lista de tarefas ao usuario

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -3,10 +3,62 @@ import { useState } from "react";
 
 const TodoApp = () => {
   //lista de tarefas
+  const [tarefas, setTarefas] = useState([]);
 
+  //estado do texto que vem do Input
+  const [valorInput, setValorInput] = useState("");
+
+  //funcão para adicionar tarefa
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    //condição para não adicionar tarefa vazia
+    if (valorInput.trim() !== "") {
+      //cria objeto com id, nome e status (concluida = false)
+      const novaTarefa = {
+        id: Date.now(),
+        nome: valorInput,
+        concluida: false,
+      };
+      //adiciona a nova tarefa à lista de tarefas e limpa o input
+      setTarefas((prevTarefa) => [...prevTarefa, novaTarefa]);
+      //limpando o input
+      setvalorInput("");
+    }
+  };
   return (
-    <div>
-      <h1 className=" text-red-500">Lista de Tarefas</h1>
+    <div className=" w-[400px] mx-auto mt-6 bg-slate-300 p-4">
+      <h1 className=" text-purple-600 text-center text-3xl my-6">
+        Lista de Tarefas
+      </h1>
+      {/* formulario para adicionar tarefas */}
+      <form className=" flex justify-between" onSubmit={handlesubmit}>
+        <input
+          className="border border-purple-200 rounded-md p-2 mr-1 w-full focus:outline-none"
+          type="text"
+          placeholder="Adicionar tarefa..."
+          value={valorInput}
+          onChange={(e) => setValorInput(e.target.value)}
+        />
+        <button
+          type="submit"
+          className=" bg-blue-600 py-2 px-6 rounded-md text-white"
+        >
+          Adicionar
+        </button>
+      </form>
+      {/* lista de tarefas */}
+      {tarefas.length === 0 && (
+        <p className="text-center py-3">Não há tarefas</p>
+      )}
+      {/* listando tarefas */}
+      <ul className="flex flex-col py-2 my-2">
+        {tarefas.map((tarefa) => (
+          <li key={tarefa.id} className="flex justify-between items-center bg-slate-200 py-2 px-2 my-2">
+            {tarefa.nome}
+            <button className="bg-red-500 text-white py-1 px-4 rounded-md">Excluir</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
