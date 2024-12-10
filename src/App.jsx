fc62730d46104c616214a19ca1e5b8b9e1e5faa6
Victor Terra <victorbterra@gmail.com>
2024-12-10 10:44:10 -0300

    feat:initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoApp from "./components/TodoApp/TodoApp";
+
+function App() {
+  return (
+    <>
+      <TodoApp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TodoApp/TodoApp.jsx
@@ -0,0 +1,9 @@
+import "./TodoApp.css";
+import React from "react";
+import { useState } from "react";
+
+const TodoApp = () => {
+  return <div>TodoApp</div>;
+};
+
+export default TodoApp;
